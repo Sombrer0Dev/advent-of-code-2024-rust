3db368d93839bc330938d714523ad0496c0c20ac
Artem Sokolov <smbrer1@gmail.com>
2024-12-06 12:47:04 +0300

    feat: finished day 3

--- src/solve/day02.rs
+++ src/solve/day02.rs
@@ -15,32 +15,66 @@ pub fn solve_day2_1(input: String) -> i64 {
     parse_input(input)
         .into_iter()
         .filter(|line| {
-            let mut order: bool = false;
-            for (i, v) in line.iter().enumerate() {
-                if i == 0 {
-                    order = line[1] > line[0]
-                } else {
-                    let p_v = line[i - 1];
-                    let dist = (p_v - v).abs();
-                    if dist < 1 || dist > 3 {
-                        return false;
-                    }
-                    if order != (*v > p_v) {
-                        return false;
-                    }
-                }
-            }
-            return true;
+            println!("{:?}", line);
+            let s = safe(line.to_vec(), false);
+            println!("{s}");
+            return s;
         })
         .count()
         .try_into()
         .unwrap()
 }
 
+fn safe(line: Vec<i64>, first: bool) -> bool {
+    let mut order: bool = false;
+    for (i, v) in line.iter().enumerate() {
+        if i == 0 {
+            order = line[1] > line[0]
+        } else {
+            let p_v = line[i - 1];
+            let dist = (p_v - v).abs();
+            if dist < 1 || dist > 3 {
+                if first {
+                    for i in 0..line.len() {
+                        let mut l_line = line.clone();
+                        l_line.remove(i);
+                        if safe(l_line, false) {
+                            return true;
+                        }
+                    }
+                    return false;
+                } else {
+                    return false;
+                }
+            }
+            if order != (*v > p_v) {
+                if first {
+                    for i in 0..line.len() {
+                        let mut l_line = line.clone();
+                        l_line.remove(i);
+                        if safe(l_line, false) {
+                            return true;
+                        }
+                    }
+                    return false;
+                } else {
+                    return false;
+                }
+            }
+        }
+    }
+    return true;
+}
 pub fn solve_day2_2(input: String) -> i64 {
-    let _ = input;
-
-    let ans: i64;
-    ans = 0;
-    return ans;
+    parse_input(input)
+        .into_iter()
+        .filter(|line| {
+            let s = safe(line.to_vec(), true);
+            println!("{:?}", line);
+            println!("{s}");
+            return s;
+        })
+        .count()
+        .try_into()
+        .unwrap()
 }
